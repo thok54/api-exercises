e3e9a5f04cf8979457967f01e2f2a8c48178450f
Jesus Alvarez <thok54@hotmail.com>
2020-03-13 20:06:12 +0100

    first commit

--- 00_start/src/API/carsApi.js
+++ 00_start/src/API/carsApi.js
@@ -1,1 +1,35 @@
-const baseUrl = 'http://localhost:3050';
+import axios from "axios";
+
+const baseUrl = "http://localhost:3050/api/cars";
+const handleError = err => console.log(err);
+
+export const getAllCars = () => {
+  return axios
+    .get(baseUrl)
+    .then(result => {
+      const cars = result.data.map(i => i);
+      resolve(cars);
+    })
+    .catch(handleError);
+};
+
+export const getCarById = id => {
+    return axios
+    .get(`${baseUrl}/2`)
+    .then(result => {
+      const car = result.data.map(i => i).find(c => c.car_id === id);
+      resolve(car);
+    })
+    .catch(handleError);
+};
+
+export const addCar = car => {
+    axios
+    .post(baseUrl)
+    .then(() => {
+      car["car_id"] = data.length + 1;
+      data.push(car);
+      resolve("ok");
+    })
+    .catch(handleError);
+};
